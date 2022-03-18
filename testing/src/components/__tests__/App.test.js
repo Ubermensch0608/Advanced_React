53d0a4f7d3287a6bb3e21fc0f11ccebaed2834da
Ubermensch0608 <melancholyee28@gmail.com>
2022-03-18 14:29:38 +0900

    Refactor: beforeEach()를 이용하여 상수 재사용 완료

--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -4,6 +4,12 @@ import App from "App.js";
 import CommentBox from "components/CommentBox.jsx";
 import CommentList from "components/CommentList.jsx";
 
+let wrapped;
+
+beforeEach(() => {
+  wrapped = shallow(<App />);
+});
+
 it("shows a comment box", () => {
   // Enzyme을 사용하여 수동으로 작성할 필요가 없어짐
   //   const div = document.createElement("div");
@@ -13,12 +19,9 @@ it("shows a comment box", () => {
   //   expect(div.innerHTML).toContain("Enter a Comment");
   //   ReactDOM.unmountComponentAtNode(div);
 
-  const wrapped = shallow(<App />);
-
   expect(wrapped.find(CommentBox).length).toEqual(1);
 });
 
 it("shows a comment list", () => {
-  const wrapped = shallow(<App />);
   expect(wrapped.find(CommentList).length).toEqual(1);
 });
